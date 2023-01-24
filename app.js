c6ef1c67fab689335bee28a31a2d224e78870c57
Scott Tippetts <wstippetts+github@gmail.com>
2023-01-24 13:22:31 -0700

    full commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// packages
+
+const packages = [{
+  priorityLevel: 'express',
+  isFragile: false,
+  weight: 2,
+  to: 'Sir Harrington IV',
+  trackingNumber: '1324kjs'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: true,
+  weight: .5,
+  to: 'Master Mercutio',
+  trackingNumber: '1325sdk'
+},
+{
+  priorityLevel: 'free',
+  isFragile: true,
+  weight: .5,
+  to: 'Mistress Ravenfeather',
+  trackingNumber: 'jffd147'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: false,
+  weight: 4,
+  to: 'B. Robert Brown',
+  trackingNumber: 'acdc145'
+},
+{
+  priorityLevel: 'express',
+  isFragile: true,
+  weight: 6,
+  to: 'Chancellor Wallace',
+  trackingNumber: '4b2l0z'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: false,
+  weight: 5,
+  to: 'Sarah Sharm',
+  trackingNumber: '8081baz'
+},
+{
+  priorityLevel: 'free',
+  isFragile: true,
+  weight: 12,
+  to: 'Tae Lien',
+  trackingNumber: 'suz2367'
+}]
+
